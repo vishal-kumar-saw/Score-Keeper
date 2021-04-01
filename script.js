6635d239c06d59b29d810a28fc30fd75e0976a62
Vishal Kumar Saw <vishalkrsaw2001@gmail.com>
2021-04-01 22:02:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const reset = document.querySelector('#reset');
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let gameOver = false;
+
+
+p1Button.addEventListener('click', () => {
+    if (!gameOver) {
+        p1Score++;
+        p1Display.textContent = p1Score;
+        if (p1Score === winningScore) {
+            gameOver = true;
+        }
+    }
+});
+
+p2Button.addEventListener('click', () => {
+    if (!gameOver) {
+        p2Score++;
+        p2Display.textContent = p2Score;
+        if (p2Score === winningScore) {
+            gameOver = true;
+        }
+    }
+});
+
+reset.addEventListener('click', () => {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    gameOver = false;
+});
