42ce44085eb9e7bd48f5acbb6a7d5f779f8d6523
Vishal Kumar Saw <vishalkrsaw2001@gmail.com>
2021-04-02 07:51:31 +0530

    add user input for winningScore and make reset function

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -30,10 +31,18 @@ p2Button.addEventListener('click', () => {
     }
 });
 
-reset.addEventListener('click', () => {
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
     gameOver = false;
-});
+}
